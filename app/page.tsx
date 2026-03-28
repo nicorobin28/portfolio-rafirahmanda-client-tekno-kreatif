8f5618055cbc080a85aa317c3b294d7d0df5339c
nicorobin28 <prayogoaskar@gmail.com>
2026-03-29 01:52:23 +0700

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,13 +9,12 @@ export default function Home() {
         <div className="flex gap-[64px]">
           <div className="flex items-end h-[84px] font-semibold w-[600px]">
             {/* <p className="text-black text-[72px]">Rafi Rahmanda</p> */}
-            <MorphText />
+            {/* <MorphText /> */}
           </div>
           <div className="flex flex-row justify-between items-end gap-[24px] h-[64px]">
             <p className="text-black text-[24px] w-[270px]">UI/UX Designer</p>
             <p className="text-black text-[24px] w-[270px]">PHNOM PENH, KH</p>
           </div>
-         
         </div>
       </main>
       <div>
