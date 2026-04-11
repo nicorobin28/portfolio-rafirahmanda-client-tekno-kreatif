736bc8b5c0cc699f29b2026768c997f6094d8835
nicorobin28 <prayogoaskar@gmail.com>
2026-04-11 17:19:22 +0700

    update changes

--- app/(main)/about/page.tsx
+++ app/(main)/about/page.tsx
@@ -34,7 +34,7 @@ const about = () => {
   };
 
   return (
-    <div className="flex gap-[20px] md:px-[120px]">
+    <div className="flex gap-[20px] px-[16px] md:px-[120px]">
       <aside className="sticky top-0 z-0 bg-[#ffffff] w-full h-screen hidden md:flex flex-col justify-start items-center pt-8">
         <div className="bg-[#ffffff]  w-[550px] h-auto flex flex-col gap-[30px]">
           {/* gap48 */}
